037f590a99d42f6056016755240e1078252c200c
Owain Perry <owain.perry@justgiving.com>
2014-01-23 13:10:41 +0000

    added vagrant

--- recipes/infrastructure_development.rb
+++ recipes/infrastructure_development.rb
@@ -2,6 +2,13 @@
 
 include_recipe "chocolatey"
 
-%w{ packer wget}.each do |pack|
+%w{ packer wget vagrant}.each do |pack|
   chocolatey pack
+end
+
+#Install vagrant windows plugin
+vagrant_windows_install_file = "c:\\windows\\vagrant-windows-installed.txt"
+execute "vagrant-windows" do
+    command "vagrant plugin install vagrant-windows > #{{vagrant_windows_install_file}}"
+    not_if { ::File.exists?(vagrant_windows_install_file)}
 end
